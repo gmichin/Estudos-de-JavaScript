4a022d1625361fa0470c73bc2f538215a05053eb
gmichin <gmassamichi@gmail.com>
2022-01-19 19:24:48 -0300

    Adicionando dados e removendo o zip

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+
 let country = "Brasil";
 let continent = "América";
 let population = 211755692;
@@ -5,3 +6,32 @@ let population = 211755692;
 console.log("País: " + country);
 console.log("Continente: " + continent);
 console.log("População: " + population);
+/*
+let isIsland;
+if (country == "Brasil") {
+    isIsland = true;
+}
+else {
+    isIsland = false;
+}
+
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = "português";
+*/
+
+console.log("Metade da população em metade do país: " + population / 2);
+population++;
+console.log("População mais 1 pessoa: " + population);
+let findland = 6000000;
+population--;
+console.log("Finlanda tem mais população que meu país? ");
+console.log(findland > country);
+console.log("Meu país tem menos pessoas do que a média mundial: ");
+console.log(country < 33000000);
+let description = "Portugal is in Europe, adn its 11 million people sepak portuguese";
