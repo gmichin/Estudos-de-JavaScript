5373f7cc95a26ccfb259ec85807d63376fa989d7
gmichin <gmassamichi@gmail.com>
2022-01-19 17:37:17 -0300

    inclusão da pasta do professor e a primeira aula

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+let country = "Brasil";
+let continent = "América";
+let population = 211755692;
+
+console.log("País: " + country);
+console.log("Continente: " + continent);
+console.log("População: " + population);
